5dad6eac44e82c37382f5b80fe331e7eb9012e6d
Ayush khare <Ayush.khare@tagbin.in>
2026-09-16 13:09:20 +0530

    Added Javascript basics

---
+++ Variables/DataTypes/dataType.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+let value = 3;
+
+console.log(value)
+ console.log(typeof(value));
+ console.log(value**value);
+ 
+ let a = 3
+ const b = ++a;
+ const c = a++;
+ // console.log(c=a+b);
+ // console.log(typeof(c));
+ 
+ 
+  // console.log("b is  = " ,b);
+ // console.log("c is  = ",c);
+  
+ console.log(2=== 2 ) 
+  
+ 
---
+++ Variables/test.js
@@ -0,0 +1,11 @@
+const accountId = 11;
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "12@12"
+city = "delhi"
+
+accountEmail="abc@gmail.com"
+accountPassword ="22@22"
+city = "bengal"
+
+console.table([accountId,accountEmail,accountPassword,city])
+
